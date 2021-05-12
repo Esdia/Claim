f492e987341ca3bc8ca54900ee614a17edf56463
Esdia <theo.hermitte@protonmail.com>
2021-05-12 17:25:18 +0200

    Added a class to test the whole model in the terminal

---
+++ tests/bobnard/claim/model/GameTest.java
@@ -0,0 +1,50 @@
+package bobnard.claim.model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GameTest {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Scanner scanner = new Scanner(System.in);
+
+        int currentPlayer;
+        ArrayList<Card> cards;
+        int n;
+
+        Card card;
+
+        while (!game.isDone()) {
+            currentPlayer = game.getCurrentPlayer();
+            cards = game.getCards(currentPlayer);
+            System.out.println("Player " + game.getCurrentPlayer() + "'s turn");
+            System.out.println(cards);
+            do {
+                System.out.print("Input card index : ");
+                n = scanner.nextInt();
+            } while (n < 0 || n >= cards.size());
+            System.out.println();
+
+            card = cards.get(n);
+            game.playCard(card);
+
+            currentPlayer = game.getCurrentPlayer();
+            cards = game.getCards(currentPlayer);
+            System.out.println("Player " + game.getCurrentPlayer() + "'s turn");
+            System.out.println(cards);
+            cards = game.getPlayableCard(currentPlayer, card.faction);
+            System.out.println("Playable cards : " + cards);
+
+            do {
+                System.out.print("Input card index : ");
+                n = scanner.nextInt();
+            } while (n < 0 || n >= cards.size());
+            System.out.println();
+
+            card = cards.get(n);
+            game.playCard(card);
+
+            game.printDebugInfo();
+        }
+    }
+}
