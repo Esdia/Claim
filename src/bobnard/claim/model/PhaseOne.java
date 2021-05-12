e9d6f84f49b022d032191f61589cb027de533c4f
Esdia <theo.hermitte@protonmail.com>
2021-05-12 17:24:00 +0200

    Implemented phase one

---
+++ src/bobnard/claim/model/PhaseOne.java
@@ -0,0 +1,51 @@
+package bobnard.claim.model;
+
+public class PhaseOne extends Phase {
+    private final Deck deck;
+
+    private Card flippedCard;
+
+    public PhaseOne(Player[] players) {
+        super(players);
+
+        this.deck = new Deck();
+        this.dealCards();
+        this.flipCard();
+    }
+
+    void dealCards() {
+        for (int i = 0; i < 13; i++) {
+            this.players[0].addCard(this.deck.draw());
+            this.players[1].addCard(this.deck.draw());
+        }
+
+        this.players[0].sortHand();
+        this.players[1].sortHand();
+    }
+
+    void flipCard() {
+        this.flippedCard = deck.draw();
+        System.out.println("Flipped card : " + this.flippedCard);
+    }
+
+    @Override
+    void endTrick() {
+        super.endTrick();
+        this.giveCentralCards();
+        this.flipCard();
+    }
+
+    void giveCentralCards() {
+        this.players[this.getLastTrickWinner()].addFollower(this.flippedCard);
+        this.players[this.getLastTrickLoser() ].addFollower(deck.draw());
+    }
+
+    @Override
+    void dealWithPlayedCards() {
+        for (Card card: this.getPlayedCards()) {
+            if (card.faction == Faction.UNDEADS) {
+                this.players[this.getLastTrickWinner()].addToScore(card);
+            }
+        }
+    }
+}
