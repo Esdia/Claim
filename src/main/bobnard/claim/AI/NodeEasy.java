b11dbbb32341456cc7663317912d7288682bf823
Esdia <theo.hermitte@protonmail.com>
2021-06-01 19:43:58 +0200

    Factorized some code

--- src/main/bobnard/claim/AI/NodeEasy.java
+++ src/main/bobnard/claim/AI/NodeEasy.java
@@ -31,34 +31,13 @@ public class NodeEasy extends Node {
         return new NodeEasy(game, aiCards, opponentPossibleCards, aiID, type);
     }
 
-    /**
-     * Heuristic depending on the difficulty.
-     * <p>
-     * Must return how much the config is in favor of the AI.
-     *
-     * @return The evaluation of the node's configuration.
-     * @see Node#evaluateState()
-     */
     @Override
-    int evaluateState() {
-        if (this.game.isDone()) {
-            /*
-             * If the game is done, it is very easy to evaluate the configuration :
-             * If the AI won, it's very good.
-             * If the AI lost, it's very bad.
-             */
-            if (this.game.getWinnerID() == this.aiID) {
-                return Integer.MAX_VALUE;
-            } else {
-                return Integer.MIN_VALUE;
-            }
-        }
+    int evaluatePhaseOne() {
+        return 0;
+    }
 
-        /*
-         * Here, we are evaluating a intermediate configuration,
-         * so we have to estimate if it is rather favorable to
-         * the AI or to the opponent.
-         */
+    @Override
+    int evaluatePhaseTwo() {
         return 0;
     }
 }
