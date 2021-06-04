ab7ea4c5e2df033f9e1163ca96450de59c3a20ee
Esdia <theo.hermitte@protonmail.com>
2021-06-04 13:44:27 +0200

    Improved the AI evaluation (now swaps player position in the middle of the testing session + show computation time and victory rate)

--- src/main/bobnard/claim/AI/EvaluateAI.java
+++ src/main/bobnard/claim/AI/EvaluateAI.java
@@ -8,25 +8,33 @@ public class EvaluateAI {
         Game game = new Game();
         Player[] players = new Player[]{
                 new AIMinimaxEasy(game, 0),
-                new AIRandom(game, 1)
+                new AIMinimaxNormal(game, 1)
         };
-        for (Player player : players) {
-            ((AI) player).setEvaluating();
+        Player[] players2 = new Player[]{
+                new AIMinimaxNormal(game, 0),
+                new AIMinimaxEasy(game, 1)
+        };
+        for (int i = 0; i < 2; i++) {
+            ((AI) players[i]).setEvaluating();
+            ((AI) players2[i]).setEvaluating();
         }
 
         game.setPlayers(players);
         game.start();
 
-        loop(game);
+        loop(game, players2);
     }
 
-    private static void loop(Game game) {
+    private static void loop(Game game, Player[] players2) {
         int[] victories = {0, 0};
+        int nbGames = 20;
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("-----------------\n\nSTARTING GAME n°" + (i+1) + "\n\n-----------------");
+        int start = (int) System.currentTimeMillis();
 
-            game.printDebugInfo();
+        for (int i = 0; i < nbGames; i++) {
+            System.out.println("-----------------\n\nSTARTING GAME n°" + (i + 1) + "\n\n-----------------");
+
+            // game.printDebugInfo();
 
             while (!game.isDone()) {
                 game.nextStep();
@@ -35,14 +43,32 @@ public class EvaluateAI {
             victories[game.getWinnerID() - 1]++;
 
             game.nextStep(); // To restart the game
+
+            if (i + 1 == nbGames / 2) {
+                System.out.println("--------");
+                System.out.println("SWAPPING");
+                System.out.println("--------");
+                game.setPlayers(players2);
+                int tmp = victories[0];
+                victories[0] = victories[1];
+                victories[1] = tmp;
+            }
+
             game.start();
         }
 
         System.out.println("------\n\nRESULT\n\n------");
 
+        double ratio;
         for (int i = 0; i < 2; i++) {
             String name = game.getPlayer(i).getClass().getSimpleName();
-            System.out.println("Player " + (i + 1) + " (" + name + ") won " + victories[i] + " games.");
+            ratio = ((double) victories[i] * 100) / nbGames;
+            System.out.println("Player " + (i + 1) + " (" + name + ") won " + victories[i] + " games. (" + ratio + "%)");
         }
+
+        int end = (int) System.currentTimeMillis();
+        int time = end - start;
+
+        System.out.println("Total execution time : " + time + " ms (" + (time / 1000) + "s).");
     }
 }
