1f0781bacc92aa6834a112750d3a7e53e6624b1c
Esdia <theo.hermitte@protonmail.com>
2021-06-04 13:41:37 +0200

    Added two other difficulties (still need balancing)

---
+++ src/main/bobnard/claim/AI/AIMinimaxNormal.java
@@ -0,0 +1,71 @@
+package bobnard.claim.AI;
+
+import bobnard.claim.model.Card;
+import bobnard.claim.model.Game;
+import bobnard.claim.model.GameState;
+
+public class AIMinimaxNormal extends AIMinimax {
+    /**
+     * Creates a new AIMinimax
+     *
+     * @param game The game on which the AI will play.
+     * @param id   The AI's player ID.
+     */
+    public AIMinimaxNormal(Game game, int id) {
+        super(game, id);
+    }
+
+    /**
+     * Returns an instance of NodeEasy
+     *
+     * @return an instance of Node
+     * @see AIMinimax#getNodeInstance()
+     */
+    @Override
+    protected Node getNodeInstance() {
+        return new NodeNormal(
+                game,
+                this.getCards(),
+                possibleOpponentCards,
+                this.getId(),
+                NodeType.MAX
+        );
+    }
+
+    /**
+     * Calculates the AI's next move in the first phase.
+     * <p>
+     * In normal mode, the AI will simply try to win every trick if
+     * the flipped card's value is at least 5.
+     *
+     * @return The index of the AI's next move in the list of
+     * it's playable cards.
+     * @see AIMinimax#nextCard()
+     */
+    @Override
+    int getMovePhaseOne() {
+        Card nextMove;
+
+        boolean want = (this.game.getFlippedCard().value >= 5);
+
+        if (game.getState() == GameState.WAITING_LEADER_ACTION) {
+            // AI plays first
+            if (want) {
+                nextMove = this.getCards().getStrongestCard();
+            } else {
+                nextMove = this.getCards().getWeakestCard();
+            }
+        } else if (game.getState() == GameState.WAITING_FOLLOW_ACTION) {
+            // AI plays second
+            if (want) {
+                nextMove = this.getCards().getWeakestToBeat(lastShownCard);
+            } else {
+                nextMove = this.getCards().getWeakestPlayableCard(lastShownCard.faction);
+            }
+        } else {
+            throw new IllegalStateException();
+        }
+
+        return this.getIndex(nextMove);
+    }
+}
---
+++ src/main/bobnard/claim/AI/NodeNormal.java
@@ -0,0 +1,67 @@
+package bobnard.claim.AI;
+
+import bobnard.claim.model.*;
+
+/**
+ * Represents a Node for an AI in normal mode.
+ */
+public class NodeNormal extends Node {
+    private static final int DEPTH = 8;
+
+    /**
+     * Creates a new Node.
+     *
+     * @param game                  The game on which the AI will play.
+     * @param aiCards               The AI's cards.
+     * @param opponentPossibleCards The cards that could be in the opponent's hand.
+     * @param aiID                  The AI's player ID
+     * @param type                  The type of the Node.
+     */
+    NodeNormal(Game game, Hand aiCards, Hand opponentPossibleCards, int aiID, NodeType type) {
+        super(game, aiCards, opponentPossibleCards, aiID, type);
+    }
+
+    /**
+     * Returns an instance of Node
+     *
+     * @return An instance of Node.
+     * @see Node#newInstance(Game, Hand, Hand, int, NodeType)
+     */
+    @Override
+    Node newInstance(Game game, Hand aiCards, Hand opponentPossibleCards, int aiID, NodeType type) {
+        return new NodeNormal(game, aiCards, opponentPossibleCards, aiID, type);
+    }
+
+    @Override
+    int getStartingDepth() {
+        return DEPTH;
+    }
+
+    /**
+     * Evaluates an intermediate configuration in phase two.
+     *
+     * @return The evaluation of the configuration
+     */
+    @Override
+    int evaluatePhaseTwo() {
+        int handVal = this.aiCards.stream().mapToInt(c -> c.value).sum();
+
+        /*
+        int handVal = this.aiCards.stream().mapToInt(c -> c.value).filter(n -> n >= 5).sum() * 2;
+        handVal += this.aiCards.stream().mapToInt(c -> c.value).filter(n -> n < 5).sum() / 2;
+         */
+
+        Player ai = this.game.getPlayer(aiID);
+        ScoreStack ss = ai.getScoreStack();
+        int scoreVal = ss.size();
+
+        for (Faction faction : Faction.values()) {
+            if (ss.getNbCardsFaction(faction) > getMaxNbCardsFaction(faction) / 2) {
+                // The AI won this faction, and cannot lose it later.
+                scoreVal += 5;
+            }
+        }
+
+        return handVal + 2 * scoreVal;
+    }
+}
